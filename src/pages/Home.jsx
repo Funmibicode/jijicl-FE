7af55dbdd7a4a5ad56a780e81a851f44a0e59de6
funmibicode <abiodunoluwafunmibi17@gmail.com>
2026-06-22 12:37:37 +0100

    Fix Navbar import - remove braces + fix path

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,13 +6,13 @@ import Navbar from '../components/NavBar'
 
 const Home = () => {
   return (
-    <>
+    <div className="bg-rose-300 text-white">
       <Navbar/>
       <div>Home page</div>
       <Card name="funmibi"> 
         <p> Home page card children components </p>
       </Card>
-    </>
+    </div>
   )
 }
 
